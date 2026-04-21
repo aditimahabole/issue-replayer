f5b6bb9b24ad8023306665dcf578197c20b0a6df
aditi.mahabole <aditi.mahabole@newgensoft.com>
2026-04-21 23:50:10 +0530

    Initial commit: Production Issue Replayer System with Spring Boot 3.5.0

---
+++ src/main/java/com/project/issue_replayer/IssueReplayerApplication.java
@@ -0,0 +1,13 @@
+package com.project.issue_replayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueReplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueReplayerApplication.class, args);
+	}
+
+}
